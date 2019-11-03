5ef277619d1eff67a9e8c00364d2cb3104f2cc4e
Zafar <cc.rafaz@zafar.cc>
2019-11-03 12:46:55 -0800

    Adding more transformations

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,3 +1,8 @@
 
 from ._misc import SubtractMean
 from ._misc import Transpose
+
+from ._misc import Transformation
+from ._misc import ToTensor, Tuplize
+from ._misc import RandomFlip
+from ._misc import Normalize
--- transforms/_misc.py
+++ transforms/_misc.py
@@ -1,3 +1,5 @@
+import copy
+import torch
 import numpy as np
 
 def get_mean(data, axis=None):
@@ -39,3 +41,61 @@ class Transpose:
       return x, y, dict()
     axis = tuple(self.axis)
     return np.transpose(x, axis), y, {}  # No need to save the transpose info.
+
+
+class Transformation:
+  def __init__(self, ts=None):
+    self.ts = ts
+  def __call__(self, sample):
+    for t in self.ts:
+      sample = t(sample)
+    return sample
+  def __iter__(self):
+    for t in self.ts:
+      yield t
+
+class ToTensor:
+  def __init__(self, device='cpu', deepcopy=True):
+    self.device = device
+    self.deepcopy = deepcopy
+  def __call__(self, sample):
+    if self.deepcopy:
+      sample = copy.deepcopy(sample)
+    sample['image'] = torch.tensor(sample['image']).permute((0, 3, 1, 2))
+    sample['image'] = sample['image'].to(torch.float)
+    sample['image'] = sample['image'].contiguous().to(self.device)
+    sample['label'] = torch.tensor(sample['label']).contiguous()
+#     sample['label'] = sample['label'].to(torch.float)
+    sample['label'] = sample['label'].to(self.device)
+    return sample
+
+class RandomFlip:
+  def __init__(self, p=0.5, axis=2):
+    self.p = p
+    self.axis = axis
+  def __call__(self, sample):
+    flip = np.random.random(sample['image'].shape[0]) >= self.p
+    sample['image'][flip] = torch.flip(sample['image'][flip],
+                                       (self.axis,))
+    return sample
+
+class Normalize:
+  def __init__(self, mean, std):
+    self.mean = mean
+    self.std = std
+  def __call__(self, sample):
+    sample['image'] = sample['image'] - self.mean
+    sample['image'] = sample['image'] / self.std
+    return sample
+  def denormalize(self, sample):
+    if isinstance(sample, dict):
+      img = sample['image']
+    else:
+      img = sample
+    img = img * self.std
+    img = img + self.mean
+    return img
+
+class Tuplize:
+  def __call__(self, sample):
+    return sample['image'], sample['label']
